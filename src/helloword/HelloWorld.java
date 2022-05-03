c7dc8122c6e500f9f7ae43f105ed21952325c7f8
yhj <1398504102@qq.com>
2022-05-03 21:17:23 +0800

    test2

--- src/helloword/HelloWorld.java
+++ src/helloword/HelloWorld.java
@@ -3,6 +3,6 @@ package helloword;
 public class HelloWorld {
  
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello WorldËæ±ã¸ÄÒ»¸Ä");
     }
 }
