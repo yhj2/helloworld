acfa88b0e95a9fbede12c5b417275ec95b06c437
yhj <1398504102@qq.com>
2022-05-03 21:17:56 +0800

    test3

--- src/helloword/HelloWorld.java
+++ src/helloword/HelloWorld.java
@@ -3,6 +3,6 @@ package helloword;
 public class HelloWorld {
  
     public static void main(String[] args) {
-        System.out.println("Hello World随便改一改");
+        System.out.println("Hello World随便改一改...");
     }
 }
